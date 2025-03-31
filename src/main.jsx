f63e1b3559c5004fac02740307e848877194c9cc
Dmytro <d.salyakin@gmail.com>
2025-04-01 02:18:49 +0300

    Integrate Redux Persist for contact storage in localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 import 'modern-normalize';
 import App from './components/App/App';
 import './index.css';
@@ -9,7 +10,9 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
